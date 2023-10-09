019cbb9028aa7589b83b90c2c1bfa901fd10fe26
Julian Houle-T <jhouletymeczko@gmail.com>
2023-10-08 21:35:49 -0400

    initialized project, started working on the footer

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      {/* <Nav></Nav> */}
+
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
